a77e11dfdfd258cad946245fa393a3d490897770
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2022-11-17 06:29:40 -0800

    Time: 2780 ms (59.06%), Space: 24.4 MB (44.88%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,30 +1,32 @@
 class NumMatrix(object):
 
     def __init__(self, matrix):
-        rows, cols = len(matrix), len(matrix[0])
-        self.sumMat =[[0]*(cols + 1) for r in range(rows +1)]
-        for r in range(rows):
-            prefix = 0
-            for c in range(cols):
-                prefix += matrix[r][c]
-                above = self.sumMat[r][c + 1]
-                self.sumMat[r+1][c+1] =prefix + above
-                
-            
         """
-        :type matrix: List[List[int]]
+        :type self.matrix: List[List[int]]
         """
+        self.matrix = matrix
+        for row in range(len(self.matrix)):
+            for col in range(1, len(self.matrix[0])):
+                self.matrix[row][col] += self.matrix[row][col - 1]
+        for row in range(1, len(self.matrix)):
+            for col in range(len(self.matrix[0])):
+                self.matrix[row][col] += self.matrix[row - 1][col]
         
-
-    def sumRegion(self, r1, c1, r2, c2):
-        r1 ,c1 ,r2, c2  = r1 +1 , c1 +1, r2+ 1, c2+ 1
-        bottomright = self.sumMat[r2][c2]
-        topright = self.sumMat[r1 -1][c2]
-        bottomleft =  self.sumMat[r2][c1 - 1]
-        topleft = self.sumMat[r1 -1][c1 - 1]
-        return  bottomright - topright - bottomleft + topleft
-
-
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        gebdaw = self.matrix[row2][col2]
+        leftMikenes = self.matrix[row2][col1 - 1] if col1 > 0 else 0
+        topMikenes = self.matrix[row1 - 1][col2] if row1 > 0 else 0
+        metdemerewa = self.matrix[row1 - 1][col1 - 1] if (col1 > 0 and row1 > 0) else 0
+        
+        return gebdaw - leftMikenes - topMikenes + metdemerewa
+        
 # Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
+# obj = NumMatrix(self.matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
